d907b38425cab9320a5f8d7ad2b8b317129e4cc3
h.overlord <richard.zevel@gmail.com>
2016-04-17 21:06:35 +0200

    Added WYSIWYG editor (CKEDITOR) + fixed XSS

--- SimpleBlogCMS/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlogCMS/Areas/Admin/Controllers/PostsController.cs
@@ -78,7 +78,7 @@ namespace SimpleBlogCMS.Areas.Admin.Controllers
             });
         }
 
-        [HttpPost, ValidateAntiForgeryToken]
+        [HttpPost, ValidateAntiForgeryToken, ValidateInput(false)]
         public ActionResult Form(PostsForm form)
         {
             form.IsNew = form.PostId == null;
